90d28e30573c7ab8c691649468dba1e6988ed2c5
suraj_markup <96483690+suraj-markup@users.noreply.github.com>
2024-09-02 14:48:17 +0530

    Update MyProjects.jsx

--- src/Components/MyProjects.jsx
+++ src/Components/MyProjects.jsx
@@ -4,7 +4,7 @@ import image3 from "../assets/image3.png";
 import image4 from "../assets/image4.png";
 import image5 from "../assets/image5.png";
 import image6 from "../assets/image6.png";
-import Circle from "../assets/circle";
+import Circle from "../assets/Circle";
 import { Link } from "react-router-dom";
 
 const MyProjects = () => {
